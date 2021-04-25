c8b1327b3a0b9ac6cc8a9ab30e83c6146c0e2862
Mostafa Ayman <mostafa.aymanae@gmail.com>
2021-04-25 02:33:45 +0200

    getCoinData implementation

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +31,22 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+const apiKey = 'F8FDA4B8-6357-4147-AAEE-22C2DA5583B7';
+const coinApiExRate = 'https://rest.coinapi.io/v1/exchangerate';
+class CoinData {
+
+  Future<dynamic> getCoinData(String base, String quote) async{
+
+    Uri url = Uri.parse('$coinApiExRate/$base/$quote?apiKey=$apiKey');
+    http.Response response = await http.get(url);
+
+    if(response.statusCode == 200)
+      return jsonDecode(response.body);
+    else
+      {
+        print(response.statusCode);
+        throw 'Problem with the get request';
+      }
+  }
+
+}
